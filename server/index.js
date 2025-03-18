4358319e95e5e8c474d61cc172c90542854eef77
Themydee <nifetemiboy@gmail.com>
2025-03-18 14:02:38 +0100

    user schema

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv"
 dotenv.config()
 import { connectDB } from "./db/connectDB.js";
 
+import authRoutes from "./routes/auth.route.js"
+
 const app = express();
 
 app.get("/", (req, res)=> {
     res.send("Hello world123!")
 })  
 
+app.use("/api/auth", authRoutes )
 app.listen(3000, () => {
     connectDB();
     console.log("server is running on port 3000")
